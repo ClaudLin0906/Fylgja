60533edb58395dc781cc42f58579c19c010ec5a4
ClaudLin <claud25315488@gmail.com>
2023-11-25 18:46:36 +0800

    no message

--- Fylgja/VerificationCode/VerificationCodeVC.swift
+++ Fylgja/VerificationCode/VerificationCodeVC.swift
@@ -20,19 +20,54 @@ class VerificationCodeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        UIInit()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func UIInit() {
+        firstTextFieldView.textField.delegate = self
+        firstTextFieldView.textField.tag = 1
+        secondTextFieldView.textField.delegate = self
+        secondTextFieldView.textField.tag = 2
+        thirdTextFieldView.textField.delegate = self
+        thirdTextFieldView.textField.tag = 3
+        fourthTextFieldView.textField.delegate = self
+        fourthTextFieldView.textField.tag = 4
     }
-    */
 
 }
+
+extension VerificationCodeVC: UITextFieldDelegate{
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // 當輸入的字不為空字串時
+        if (string != "") {
+            //當textField不為空字串時
+            if (textField.text == "") {
+                textField.text = string
+                //建立一個響應者為目前textField tag的下一個
+                let nextResponder: UIResponder? = view.viewWithTag(textField.tag + 1)
+                //只要響應者不是nil就讓他成為第一位響應者這樣就可以連續輸入時自動切到下一格
+                if (nextResponder != nil) {
+                    nextResponder?.becomeFirstResponder()
+                }else{
+                //當tag到最後時響應者會是nil此時執行將鍵盤收起的function
+                    self.view.endEditing(true)
+                    let smsCodeStr = (firstTextFieldView.textField.text ?? "") + (secondTextFieldView.textField.text ?? "") + (thirdTextFieldView.textField.text ?? "") + (fourthTextFieldView.textField.text ?? "")
+                    print(smsCodeStr)
+                }
+            }
+            return false
+        } else {//當我們按下刪除鍵時相當於輸入空字串
+            textField.text = string
+            //建立一個tag往前的響應者
+            let nextResponder: UIResponder? = view.viewWithTag(textField.tag - 1)
+            if (nextResponder != nil) {
+            //只要響應者不是nil就讓他成為第一位響應者這樣就可以連續輸入時自動切到上一格
+                nextResponder?.becomeFirstResponder()
+            }
+            return false
+        }
+    }
+}
+
